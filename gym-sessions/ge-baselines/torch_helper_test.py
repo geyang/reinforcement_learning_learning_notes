b0ec98cb157358386c844a0f86be0182ce5f12cd
Ge Yang <yangge1987@gmail.com>
2017-08-05 23:05:37 -0700

    * FIX torch_helper critical bugs
    Took 87 hours 15 minutes

--- gym-sessions/ge-baselines/torch_helper_test.py
+++ gym-sessions/ge-baselines/torch_helper_test.py
@@ -1,16 +1,19 @@
 import torch_helpers as h
+import numpy as np
 
 
 def test_one_hot():
-    acts = h.varify([[1], [2]], dtype='int')
+    acts = h.varify([1, 2], dtype='int')
     n = 3
     oh = h.one_hot(acts, n)
-    print(oh)
+    assert (oh.data.numpy() == np.array([[0, 1, 0], [0, 0, 1]])).all(), "one_hot gives incorrect output {}".format(oh)
+
 
 
 def test_mask():
     probs = h.varify([[0.1, 0.2, 0.7], [0.4, 0.5, 0.1]], requires_grad=True)
     acts = h.varify([1, 2], dtype='int')
-    oh = h.sample_probs(probs, acts)
-    oh.sum().backward()
-    print(oh)
+    sampled_probs = h.sample_probs(probs, acts)
+    sampled_probs.sum().backward()
+    dp = probs.grad.data.numpy()
+    assert dp[0, 1] is not None and dp[1, 2] is not None, 'key entries of probs grad should be non-zero'
--- gym-sessions/ge-baselines/torch_helpers.py
+++ gym-sessions/ge-baselines/torch_helpers.py
@@ -36,19 +36,32 @@ def varify(d, dtype='float', **kwargs) -> Variable:
     return Variable(t, **kwargs)
 
 
-def sample_probs(probs, sampled, dim=-1):
+def sample_probs(probs, index, dim=-1):
     """
     :param probs: Size(batch_n, feat_n)
-    :param sampled: Size(batch_n, ), Variable(LongTensor)
+    :param index: Size(batch_n, ), Variable(LongTensor)
     :param dim: integer from probs.size inclusive.
     :return: sampled_probability: Size(batch_n)
 
-    Use scatter and allow the gradient to flow from output -> probs.
+    NOTE: scatter does not propagate grad to source. Use mask_select instead.
+
+    Is similar to `[var[row,col] for row, col in enumerate(index)]`.
+    ```
+    import torch
+    import torch_extras
+    setattr(torch, 'select_item', torch_extras.select_item)
+    var = torch.range(0, 9).view(-1, 2)
+    index = torch.LongTensor([0, 0, 0, 1, 1])
+    torch.select_item(var, index)
+    # [0, 2, 4, 7, 9]
+    ```
     """
-    # we do not need to unsqueeze and expand input here.
-    assert probs.size()[:-1] == sampled.size(), 'sampled should be 1 less dimension than probs.'
-    zeros = Variable(torch.zeros(*probs.size()))
-    return zeros.scatter(dim, sampled.unsqueeze(dim=-1).expand_as(probs), probs).sum(dim=-1)
+
+    assert probs.size()[:-1] == index.size(), 'index should be 1 less dimension than probs.'
+    mask = one_hot(index, probs.size()[-1])
+    mask = cast(mask, 'byte').detach()
+    mask.requires_grad = False
+    return torch.masked_select(probs, mask).view(*probs.size()[:-1])
 
 
 # done: change type to Variable from Tensor
@@ -61,5 +74,41 @@ def one_hot(index: Variable, feat_n: int):
     """
     zeros = Variable(torch.FloatTensor(*index.size(), feat_n).zero_())
     ones = Variable(torch.ones(*zeros.size()))
-    zeros.scatter(-1, index.unsqueeze(-1).expand_as(zeros), ones)
-    return zeros
+    return zeros.scatter(-1, index.unsqueeze(-1).expand_as(zeros), ones)
+
+
+TYPES = {
+    "byte": "byte",
+    torch.ByteTensor: "byte",
+    "str": "char",
+    torch.CharTensor: "char",
+    "double": "double",
+    torch.DoubleTensor: "double",
+    "float": "float",
+    torch.FloatTensor: "float",
+    "int": "int",
+    torch.IntTensor: "int",
+    "long": "long",
+    torch.LongTensor: "long",
+    "short": "short",
+    torch.ShortTensor: "short"
+}
+
+
+def cast(var, dtype):
+    """ Cast a Tensor to the given type.
+        ```
+        import torch
+        import torch_extras
+        setattr(torch, 'cast', torch_extras.cast)
+        input = torch.FloatTensor(1)
+        target_type = type(torch.LongTensor(1))
+        type(torch.cast(input, target_type))
+        # <class 'torch.LongTensor'>
+        ```
+    """
+
+    if dtype in TYPES:
+        return getattr(var, TYPES[dtype])()
+    else:
+        raise ValueError("Not a Tensor type.")
